dd948cd63862d652c4ab964cbe6639fda9f4ea27
jflorimo <flojeanc@yahoo.fr>
2022-02-13 12:17:10 +0100

    Update template

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/types/**/*.ts'],
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
+};
